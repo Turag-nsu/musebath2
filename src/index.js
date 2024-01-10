caadb4804afa09e3d6143ff2e2f469ea88a9bb4c
nayem921 <abdnayem2019@gmail.com>
2024-01-11 04:45:58 +0600

    snap remove

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,13 +12,13 @@ root.render(
   </React.StrictMode>
 );
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-}
-else {
-  render(<App />, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// }
+// else {
+//   render(<App />, rootElement);
+// }
 
 
 // If you want to start measuring performance in your app, pass a function
