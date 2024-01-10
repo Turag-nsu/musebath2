0c02c87ea53c3df3614114be20c2181dc8096823
nayem921 <abdnayem2019@gmail.com>
2024-01-11 01:43:51 +0600

    meta tag added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { hydrate, render } from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+}
+else {
+  render(<App />, rootElement);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
