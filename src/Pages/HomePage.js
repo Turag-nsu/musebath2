90d573578e9215069dc59048b28ff26f08b2387a
nayem921 <abdnayem2019@gmail.com>
2024-01-10 19:45:31 +0600

    meta tag added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Helmet } from "react-helmet";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import HeroPart from '../components/HeroPart/HeroPart';
@@ -13,10 +14,10 @@ import ArticleArea from '../components/ArticleArea/ArticleArea';
 import TrustedArea from '../components/TrustedArea/TrustedArea';
 import CTAArea from '../components/CTAArea/CTAArea';
 
-import {handleGetProjects} from '../services/projectServices';
+import { handleGetProjects } from '../services/projectServices';
 import { useState } from 'react';
 
-const HomePage=()=> {
+const HomePage = () => {
   const [images, setImages] = useState([]);
   const getImages = async () => {
     const { data } = await handleGetProjects();
@@ -53,17 +54,43 @@ const HomePage=()=> {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Musebath - Premier Bathroom Remodeling Services in Missouri</title>
+        <meta data-rh="true" name="description" content="Transform your bathroom with the trusted and best bathroom remodeling services in Missouri, USA. Musebath, your local expert, brings innovative designs and top-notch craftsmanship. Redefine your space with America's premier choice. Near you, we excels in enhancing bathroom interiors, ensuring excellence in every detail" />
+        <meta name="keywords" content="Bathroom Remodeling Missouri, Trusted Bathroom Renovations, Best Bathroom Interior Design, Top-Rated Remodelers in USA, Expert Bathroom Contractors, Missouri's Premier Bath Renovations, America's Trusted Bathroom Remodels, Near Me Bathroom Upgrades, Best USA Bathroom Makeovers, Musebath Excellence in Remodeling"/>
+
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Muse Bathrooms" />
+        <meta name="distribution" content="global" />
+        <meta name="rating" content="general" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="language" content="en" />
+        <meta name="url" content="https://www.musebath.us/" />
+        <meta name="identifier-URL" content="https://www.musebath.us/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="sKwhJT5VSAlST2YHkJ1Stbt28IRiU5Qj1pzpbGo7Syc" />
+        <meta property="og:title" content="Muse Bathrooms | Bathroom Renovations in Missouri" />
+        <meta property="og:description" content="Muse Bath is a family owned and operated business that has been in the industry for over 30 years. We specialize in bathroom renovations in Missouri." />
+        <meta property="og:image" content="../Images/hero-bg.png" />
+        {/* <meta property="og:url" content="https://musebath.us/my-page" /> */}
+        {/* <meta name="twitter:title" content="Muse Bathrooms | Bathroom Renovations in Missouri" />
+        <meta name="twitter:description" content="Muse Bath is a family owned and operated business that has been in the industry for over 30 years. We specialize in bathroom renovations in Missouri." />
+        <meta name="twitter:image" content="https://example.com/image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@TwitterUser" /> */}
+
+      </Helmet>
       {/* <CustomNavbar /> */}
-       <div style={{ backgroundColor: '#F8F9FA' }}>
+      <div style={{ backgroundColor: '#F8F9FA' }}>
         <HeroPart />
       </div>
-      
+
       <div style={{ backgroundColor: '#F8F9FA' }}>
         <WhyChoosePart />
       </div>
-      <div 
-      id='services'
-      style={{ backgroundColor: '#FFF' }}>
+      <div
+        id='services'
+        style={{ backgroundColor: '#FFF' }}>
         <ServiceArea />
       </div>
       <div style={{ backgroundColor: '#F8F9FA' }}>
@@ -73,7 +100,7 @@ const HomePage=()=> {
         <WorkArea />
       </div>
       <div style={{ backgroundColor: '#F8F9FA' }}>
-        <GallaryArea images={images} title="Bathroom Remodel Showcase"/>
+        <GallaryArea images={images} title="Bathroom Remodel Showcase" />
       </div>
       <div style={{ backgroundColor: '#FFF' }}>
         <ServiceArea2 />
@@ -90,7 +117,7 @@ const HomePage=()=> {
       <div style={{ backgroundColor: '#FFF' }}>
         <CTAArea />
       </div>
-      </div>
+    </div>
   );
 }
 
